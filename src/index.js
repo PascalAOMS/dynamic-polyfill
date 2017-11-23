2f315e6c450a3f4d6a6dd17e7f179602a3cb3c6b
Pascal Klau <pku@sybit.de>
2017-11-23 13:36:16 +0100

    replace forEach() w/ for(); some tidying up

--- src/index.js
+++ src/index.js
@@ -1,64 +1,54 @@
 function polyfill({ fills, options = '', minify = true, rum = true, afterFill }) {
 
+  // contains list of all to be polyfilled features
+  let needsPolyfill = [];
 
-    let needsPolyfill = [] // contains list of all to be polyfilled features
+  // check if 'always' flag is set
+  // if yes, no need to check for support since polyfills are loaded anyway
+  if (options.indexOf('always') === -1) {
 
+    if (fills !== undefined) {
 
-    // check if 'always' flag is set
-    // if yes, no need to check for support since polyfills are loaded anyway
-    if( options.indexOf('always') === -1 ) {
+      for (let i = 0; i < fills.length; i++) {
+        const fill = fills[i];
 
+        // set the fill against the window object
+        const reducedFill = (fill.indexOf('~') !== -1)
+          ? window[fill.split('.')[0]]
+          : fill.split('.').reduce((k, v) => k[v], window);
 
-        if( fills !== undefined ) {
-
-            fills.forEach(fill => {
-
-                // set the fill against the window object
-                var reducedFill = (fill.indexOf('~') !== -1)
-                    ? window[fill.split('.')[0]]
-                    : fill.split('.').reduce(function (k, v) {
-                        return k[v];
-                    }, window)
-
-                // check if window supports the fill
-                if( reducedFill === undefined ) {
-                    needsPolyfill.push(fill)
-                }
-
-            })
-
-            // if no polyfills are needed, run callback
-            if( needsPolyfill.length === 0 ) {
-                afterFill()
-                return
-            }
+        // check if window supports the fill
+        if (reducedFill === undefined) {
+          needsPolyfill.push(fill);
         }
 
-    } else {
-        needsPolyfill = fills
+      }
+
+      // if no polyfills are needed, run callback
+      if (needsPolyfill.length === 0) {
+        return afterFill();
+      }
     }
 
+  } else {
+    needsPolyfill = fills
+  }
 
-    let min      = minify ? '.min' : '',
+  const min      = minify  ? '.min' : '';
+  const features = fills   ? `features=${needsPolyfill.join(',')}` : '';
+  const flags    = options ? `\&flags=${options.join(',')}` : '';
+  const monitor  = rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads scripts anyway
 
-        features = fills ? `features=${needsPolyfill.join(',')}` : '',
+  const js = document.createElement('script');
 
-        flags    = options ? `\&flags=${options.join(',')}` : '',
+  js.src = `https://cdn.polyfill.io/v2/polyfill${min}.js?${features + flags + monitor}`;
+  js.async = true;
 
-        monitor  = rum ? '\&rum=1' : '', // not set to rum=0 since it loads scripts anyway
+  document.body.appendChild(js);
 
-        js       = document.createElement('script')
-
-
-    js.src = `https://cdn.polyfill.io/v2/polyfill${min}.js?${features + flags + monitor}`
-    js.async = true
-
-    document.body.appendChild(js)
-
-    js.onload = () => afterFill()
-    js.onerror = () => afterFill(new Error('Failed to load polyfill. Please write an issue here: https://github.com/PascalAOMS/dynamic-polyfill/issues', js.src))
-
+  js.onload = () => afterFill();
+  js.onerror = () => afterFill(new Error('Failed to load polyfill. Please write an issue here: https://github.com/PascalAOMS/dynamic-polyfill/issues', js.src));
 
 }
 
-module.exports = polyfill
+module.exports = polyfill;
