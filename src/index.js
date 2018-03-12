b1eb18d58d3bc9b98681eb5a553a74fe6b20b7c7
Pascal Klau <pku@sybit.de>
2018-03-12 10:05:57 +0100

    fix Element bug, update Babel, refactoring

--- src/index.js
+++ src/index.js
@@ -17,26 +17,7 @@ function polyfill({
     }
 
     for (let i = 0; i < fills.length; i++) {
-      const fill  = fills[i];
-      const parts = fill.split('.');
-
-      const isIntl    = parts[0] === 'Intl';  // check for Intl.~locale
-      const isElement = parts[0] === 'Element';  // for Element.prototype.classList
-
-      let isSupported = false;
-
-      if (isIntl) {
-        isSupported = window[parts[0]];
-      } else if (isElement) {
-        isSupported = ('Element' in window && 'classList' in Element.prototype);
-      } else {
-        parts.reduce((key, value) => key[value], window);
-      }
-
-      if (!isSupported) {
-        neededPolyfills.push(fill);
-      }
-
+      checkFillSupport(neededPolyfills, fills[i], fills[i].split('.'));
     }
 
     // if no polyfills are needed, run callback
@@ -47,10 +28,41 @@ function polyfill({
 
   }
 
-  const min      = minify  ? '.min' : '';
-  const features = fills   ? `features=${neededPolyfills.join(',')}` : '';
-  const flags    = options ? `\&flags=${options.join(',')}` : '';
-  const monitor  = rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads scripts anyway
+  loadScript({ neededPolyfills, minify, fills, options, rum, afterFill });
+}
+
+
+function checkFillSupport(neededPolyfills, fill, parts) {
+  const isIntl    = parts[0] === 'Intl';  // check for Intl.~locale
+  const isElement = parts[0] === 'Element';  // for Element.prototype.classList
+  const isPrototype = parts[1] === 'prototype';
+
+  let isSupported = false;
+
+  // check for special test cases, otherwise use regular reduce function against window
+  if (isIntl) {
+    isSupported = window[parts[0]];
+  } else if (isElement) {
+    if (isElement && isPrototype) {
+      isSupported = ('Element' in window && parts[2] in Element.prototype);
+    } else {
+      isSupported = 'Element' in window;
+    }
+  } else {
+    isSupported = parts.reduce((key, value) => key[value], window);
+  }
+
+  if (!isSupported) {
+    return neededPolyfills.push(fill);
+  }
+}
+
+
+function loadScript(args) {
+  const min      = args.minify  ? '.min' : '';
+  const features = args.fills   ? `features=${args.neededPolyfills.join(',')}` : '';
+  const flags    = args.options ? `\&flags=${args.options.join(',')}` : '';
+  const monitor  = args.rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads scripts anyway
 
   const js = document.createElement('script');
 
@@ -59,9 +71,9 @@ function polyfill({
 
   document.body.appendChild(js);
 
-  js.onload = () => afterFill();
-  js.onerror = () => afterFill(new Error('Failed to load polyfill. Please write an issue here: https://github.com/PascalAOMS/dynamic-polyfill/issues', js.src));
-
+  js.onload = () => args.afterFill();
+  js.onerror = () => args.afterFill(new Error('Error loading polyfill. Write an issue: https://github.com/PascalAOMS/dynamic-polyfill/issues', js.src));
 }
+
 
 module.exports = polyfill;
