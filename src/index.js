a99cd0bde92bc038b7c68eb39bd0bc2ddabb1b57
Pascal Klau <pku@sybit.de>
2018-03-13 16:03:37 +0100

    add agent + fallback options; fix error handling

--- src/index.js
+++ src/index.js
@@ -3,58 +3,63 @@ function polyfill({
   options = '',
   minify = true,
   rum = true,
+  agent,
+  agentFallback,
   afterFill
 }) {
-  const fillAlways = options.indexOf('always') >= 0;   // check if 'always' flag is set
-
-  let neededPolyfills = []; // contains list of all features to be polyfilled
-
-  if (fillAlways) {
-    neededPolyfills = fills
-  } else {
-    if (fills === undefined) {
-      return new Error('No fills specified.')
-    }
-
-    for (let i = 0; i < fills.length; i++) {
-      checkFillSupport(neededPolyfills, fills[i], fills[i].split('.'));
-    }
-
-    // if no polyfills are needed, run callback
-    if (neededPolyfills.length === 0) {
-      return afterFill();
-    }
-
-
+  if (!fills) {
+    throw new Error('No fills specified.');
   }
 
-  loadScript({ neededPolyfills, minify, fills, options, rum, afterFill });
+  const fillAnyway = options.indexOf('always') >= 0 || agent; // check if 'always' flag or agent is set
+  const neededPolyfills = fillAnyway ? fills : checkSupport(fills);
+
+
+  if (neededPolyfills.length > 0) {
+    return loadScript({
+      neededPolyfills, minify, fills, options, rum, agent, agentFallback, afterFill
+    });
+  }
+
+  return afterFill();
 }
 
 
-function checkFillSupport(neededPolyfills, fill, parts) {
-  const isIntl    = parts[0] === 'Intl';  // check for Intl.~locale
-  const isElement = parts[0] === 'Element';  // for Element.prototype.classList
-  const isPrototype = parts[1] === 'prototype';
+function checkSupport(fills) {
+  let unsupportedFills = [];
 
-  let isSupported = false;
+  for (let i = 0; i < fills.length; i++) {
+    const fill = fills[i];
+    const parts = fill.split('.'); // i.e. ['Array', 'prototype', 'includes']
+    const type = parts[0];
+    const prototype = parts[1] === 'prototype';
+    const method = parts[2];
 
-  // check for special test cases, otherwise use regular reduce function against window
-  if (isIntl) {
-    isSupported = window[parts[0]];
-  } else if (isElement) {
-    if (isElement && isPrototype) {
-      isSupported = ('Element' in window && parts[2] in Element.prototype);
-    } else {
-      isSupported = 'Element' in window;
+    let isSupported = false;
+
+    // check for special test cases, otherwise use regular reduce function against window
+    switch (type) {
+      case 'Intl':
+        isSupported = window[type];
+        break;
+
+      case 'Element':
+        isSupported = 'Element' in window;
+        if (prototype && isSupported) {
+          isSupported = method in Element.prototype;
+        }
+        break;
+
+      default:
+        isSupported = parts.reduce((key, value) => key[value], window);
     }
-  } else {
-    isSupported = parts.reduce((key, value) => key[value], window);
+
+    if (!isSupported) {
+      unsupportedFills.push(fill);
+    }
   }
 
-  if (!isSupported) {
-    return neededPolyfills.push(fill);
-  }
+  return unsupportedFills;
 }
 
 
@@ -62,17 +67,19 @@ function loadScript(args) {
   const min      = args.minify  ? '.min' : '';
   const features = args.fills   ? `features=${args.neededPolyfills.join(',')}` : '';
   const flags    = args.options ? `\&flags=${args.options.join(',')}` : '';
-  const monitor  = args.rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads scripts anyway
+  const monitor  = args.rum     ? '\&rum=1' : ''; // not set to rum=0 since it loads RUM scripts anyway
+  const agent    = args.agent   ? `\&ua=${args.agent}` : '';
+  const fallback = args.agentFallback ? `\&unknown=${args.agentFallback}` : '';
 
   const js = document.createElement('script');
 
-  js.src = `https://cdn.polyfill.io/v2/polyfill${min}.js?${features + flags + monitor}`;
+  js.src = `https://cdn.polyfill.io/v2/polyfill${min}.js?${features + flags + monitor + agent + fallback}`;
   js.async = true;
 
   document.body.appendChild(js);
 
   js.onload = () => args.afterFill();
-  js.onerror = () => args.afterFill(new Error('Error loading polyfill. Write an issue: https://github.com/PascalAOMS/dynamic-polyfill/issues', js.src));
+  js.onerror = () => { throw new Error('Error loading polyfills. Open a ticket: https://github.com/PascalAOMS/dynamic-polyfill/issues') };
 }
 
 
